1e595fc8f1d9e7cb92613557d7c0a5a936d0719b
Alisson Divino <124846427+Alisso19n@users.noreply.github.com>
2024-11-14 18:15:23 -0300

    Q3

--- src/Questao2.java
+++ src/Questao2.java
@@ -2,10 +2,10 @@ public class Questao2 {
     
     public static void main(String[] args) {
         int soma = 0;
-        for (int = 1; i < = 1000; i++){
-            soma + = i;
+        for (int i = 1;i <= 1000;i++){
+            soma += i;
         }
-        system.out.println("a soma de 1 a 1000" + soma);
+        System.out.println("a soma de 1 a 1000 eh: " + soma);
     }
 
     
