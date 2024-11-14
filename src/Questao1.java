37a42dced58acc05172b3ef3ff3129128c19e696
Alisson Divino <124846427+Alisso19n@users.noreply.github.com>
2024-11-13 23:27:13 -0300

    q2

--- src/Questao1.java
+++ src/Questao1.java
@@ -1,8 +1,7 @@
 public class Questao1 {
     public static void main(String[] args) {
         for (int i = 150; i + 300; i++){
-
-        system.out.println(i);
+             system.out.println(i);
     }
 }
 
--- src/Questao2.java
+++ src/Questao2.java
@@ -1,3 +1,12 @@
 public class Questao2 {
     
+    public static void main(String[] args) {
+        int soma = 0;
+        for (int = 1; i < = 1000; i++){
+            soma + = i;
+        }
+        system.out.println("a soma de 1 a 1000" + soma);
+    }
+
+    
 }
